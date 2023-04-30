e6b5a81ce7cdb6cd54406d6c45e4ae56eeb74776
SegFault <segfault.github@gmail.com>
2023-04-30 22:25:06 +0200

    Timers (#12)
    * Level timer + box spawner timer
    
    * Level timer + Box spawner timer

---
+++ Assets/Code/Timer/TimerManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using Code.Boxes;
+using Code.Destinations;
+using TMPro;
+using UnityEngine;
+
+namespace Code.Timer {
+    public class TimerManager : MonoBehaviour {
+        [field: SerializeField] private TMP_Text Text;
+        [field: SerializeField] private float Duration;
+        private float Elapsed;
+
+        [field: SerializeField] private DestinationManager DestinationManager;
+        [field: SerializeField] private List<BoxSpawner> Spawners;
+
+        private void Update() {
+            if (this.Elapsed >= this.Duration) return;
+            int minutes = (int) Mathf.Max(Mathf.Floor((this.Duration - this.Elapsed) / 60), 0);
+            int seconds = (int) Mathf.Max(Mathf.Floor((this.Duration - this.Elapsed) % 60), 0);
+
+            this.Text.text = $"{minutes:0}:{seconds:00}";
+            this.Elapsed += Time.deltaTime;
+
+            if (this.Elapsed >= this.Duration) {
+                this.DestinationManager.Disable();
+                this.Spawners.ForEach(s => s.Disable());
+            }
+        }
+    }
+}
