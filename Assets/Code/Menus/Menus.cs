87b34d53cb05360a91341ff55cbf51fd11a5aea5
Deloriand <deloprod@gmail.com>
2023-04-30 19:49:01 +0200

    Creation of the level selection menu

--- Assets/Code/Menus/Menus.cs
+++ Assets/Code/Menus/Menus.cs
@@ -4,15 +4,22 @@ using UnityEngine.SceneManagement;
 public class Menus : MonoBehaviour {
 
     public GameObject settingsMenu;
+    public GameObject levelsSelectionMenu;
+
     private bool settingsOpen;
+    private bool levelSelectionOpen;
 
     private void Start() {
         this.settingsOpen = false;
+        levelSelectionOpen = false;
         this.settingsMenu.SetActive(this.settingsOpen);
+        this.levelsSelectionMenu.SetActive(this.levelSelectionOpen);
     }
 
     public void PlayGame() {
-        SceneManager.LoadScene(1);
+        //SceneManager.LoadScene(1);
+
+        LevelsSelectionMenu();
     }
 
     public void QuitGame() {
@@ -22,5 +29,11 @@ public class Menus : MonoBehaviour {
     public void SettingsMenu() {
         this.settingsOpen = !this.settingsOpen;
         this.settingsMenu.SetActive(this.settingsOpen);
+    }
+
+    public void LevelsSelectionMenu()
+    {
+        this.levelSelectionOpen = !this.levelSelectionOpen;
+        this.levelsSelectionMenu.SetActive(this.levelSelectionOpen);
     }
 }
